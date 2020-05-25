a1eb2ef6c73efe59133919dea00ec58dea6dde8e
Senthil Palani <senthil.palani@outlook.com>
2020-05-25 12:19:43 +0530

    initial commit

---
+++ Video.Conferencing/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Video.Conferencing
+{
+    public class AppSettings
+    {
+        public string TwilioAccountSid { get; set; }
+        public string TwilioApiKeySid { get; set; }
+        public string TwilioApiKeySecret { get; set; }
+    }
+}
---
+++ Video.Conferencing/Controllers/TwilioController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Jwt.AccessToken;
+
+namespace Video.Conferencing.Controllers
+{
+    [Route("token")]
+    [ApiController]
+    public class TwilioController : ControllerBase
+    {
+        private readonly AppSettings appSettings;
+
+        public TwilioController(AppSettings appSettings)
+        {
+            this.appSettings = appSettings;
+        }
+
+        [HttpGet]
+        public ActionResult Get(string identity)
+        {
+            // Create an Chat grant for this token
+            var grant = new VideoGrant();
+            var grants = new HashSet<IGrant> { { grant } };
+
+            // Create an Access Token generator
+            var token = new Token(
+               appSettings.TwilioAccountSid,
+               appSettings.TwilioApiKeySid,
+               appSettings.TwilioApiKeySecret,
+                identity,
+                grants: grants);
+
+            return Ok(token.ToJwt());
+        }
+    }
+}
